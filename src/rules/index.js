80499949863d2e4504e05bae0cf702ac90812deb
Domingas Pleckauskas <d.pleck@outlook.com>
2026-02-16 09:20:54 +0000

    refactor: migrate limited-inline-classes to jsdoc

--- src/rules/index.js
+++ src/rules/index.js
@@ -1,8 +1,9 @@
+import { rule as limitedInlineClasses } from "./limited-inline-classes.js";
 import { rule as requireVariantsCallStylesName } from "./require-variants-call-styles-name.js";
 import { rule as requireVariantsSuffix } from "./require-variants-suffix.js";
 
 export const rules = {
-  // "limited-inline-classes": limitedInlineClasses,
+  "limited-inline-classes": limitedInlineClasses,
   "require-variants-call-styles-name": requireVariantsCallStylesName,
   "require-variants-suffix": requireVariantsSuffix,
   // "sort-custom-properties": sortCustomProperties,
---
+++ src/utils/create-rule-visitors.js
@@ -0,0 +1,28 @@
+/**
+ * Create and return rule visitors that work for both Vue single-file components
+ * and regular script files.
+ * @param {import("eslint").Rule.RuleContext} context
+ * @param {import("eslint").Rule.RuleListener} templateVisitor
+ * @param {import("eslint").Rule.RuleListener} scriptVisitor
+ * @returns {import("eslint").Rule.RuleListener} Appropriate visitor based on the file type.
+ */
+export const createRuleVisitors = (context, templateVisitor, scriptVisitor) => {
+  const fileName = context.filename;
+
+  if (fileName.endsWith(".vue")) {
+    const { sourceCode } = context;
+    const { parserServices } = sourceCode;
+
+    if (
+      parserServices !== "undefined" &&
+      "defineTemplateBodyVisitor" in parserServices
+    ) {
+      return parserServices.defineTemplateBodyVisitor(
+        templateVisitor,
+        scriptVisitor,
+      );
+    }
+  }
+
+  return scriptVisitor;
+};
